a93491fcf0806ee0a23ce7c45c43b350db71b8e7
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-03-14 10:30:23 +0300

    [#22] Check periodically: improved showing info

--- src/providers/shutdowns.py
+++ src/providers/shutdowns.py
@@ -1,3 +1,4 @@
+import logging
 import datetime
 from typing import NamedTuple, Type
 
@@ -5,14 +6,20 @@ from src.config.app import SupportedService, SupportedCity, DT_FORMAT, D_FORMAT
 from src.parsing.data_models import Address
 from src.parsing.main_parsing import BaseParser
 
+logger = logging.getLogger(__name__)
+
 
 class ShutDownInfo(NamedTuple):
     start: datetime.datetime | datetime.date | None
     end: datetime.datetime | datetime.date | None
     raw_address: str
     city: SupportedCity
+    error: str | None = None
 
     def __str__(self) -> str:
+        if self.error:
+            return f"{self.raw_address}: unable to get ({self.error})"
+
         return f"{self.raw_address}: {self.dt_format(self.start)} - {self.dt_format(self.end)}"
 
     @classmethod
@@ -51,12 +58,24 @@ class ShutDownProvider:
         user_address = Address.from_string(raw_address=address)
         parser_class: Type[BaseParser] = BaseParser.get_parsers()[service]
         parser = parser_class(city)
-        shutdowns = parser.parse(user_address=user_address)
-        print(f"{shutdowns=}")
         result: list[ShutDownInfo] = []
 
+        try:
+            shutdowns = parser.parse(user_address=user_address)
+        except Exception as exc:
+            logger.exception("Could not fetch shutdowns: %r", exc)
+            result.append(
+                ShutDownInfo(
+                    start=None,
+                    end=None,
+                    raw_address=address,
+                    city=city,
+                    error=str(exc),
+                )
+            )
+            return result
+
         for address_, data_ranges in shutdowns.items():
-            print(address, data_ranges)
             for data_range in data_ranges:
                 result.append(
                     ShutDownInfo(
@@ -85,11 +104,12 @@ class ShutDownProvider:
         """
         shutdown_info_list = []
         for service in SupportedService.members():
-            print(f"{service=}")
             for address in addresses:
                 if shutdowns := cls.for_address(SupportedCity.SPB, address, service):
                     shutdown_info_list.append(
                         ShutDownByServiceInfo(service=service, shutdowns=shutdowns)
                     )
+                    if shutdowns[0].error:
+                        break
 
         return shutdown_info_list
